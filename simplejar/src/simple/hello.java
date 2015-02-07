f425739393ec834d90b6e19a8deed0c0a93ae853
illakya <illakyaramesh@gmail.com>
2015-02-07 12:43:35 +0530

    firsttime commit

---
+++ simplejar/src/simple/hello.java
@@ -0,0 +1,13 @@
+package simple;
+
+public class hello {
+
+	public static void displayHello()
+	{
+		System.out.println("display Hello");
+	}
+		public static void main(String[] args)
+		{
+		
+		}
+	}
