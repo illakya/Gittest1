65de390199411f596c8bc69cd43b472cf1be471a
illakya <illakyaramesh@gmail.com>
2015-02-07 12:49:10 +0530

    secondtime commit

--- simplejar/src/simple/hello.java
+++ simplejar/src/simple/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void displayHello()
 	{
 		System.out.println("display Hello");
+		System.out.println("Hello2");
 	}
 		public static void main(String[] args)
 		{
